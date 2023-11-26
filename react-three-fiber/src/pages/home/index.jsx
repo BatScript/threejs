3d85042c3b30831de0dfbb37e09b7210bf7cec5d
Mohit <mohit.ranjan@lybrate.com>
2023-11-26 11:16:48 +0530

    react three fiber

---
+++ react-three-fiber/src/pages/home/index.jsx
@@ -0,0 +1,11 @@
+import HomePageContainer from '@page-containers/home'
+
+const Home = () => {
+  return (
+    <div>
+      <HomePageContainer />
+    </div>
+  )
+}
+
+export default Home
--- react-three-fiber/vite.config.js
+++ react-three-fiber/vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: [
+      { find: '@pages', replacement: '/src/pages' },
+      { find: '@components', replacement: '/src/components' },
+      { find: '@page-containers', replacement: '/src/page-containers' }
+    ]
+  }
 })
